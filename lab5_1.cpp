7cc7e79dcb90c272b2c89ecaab81dc86e956acf7
lab302-8 <lab302-8@labeng.local>
2020-01-07 17:03:56 +0700

    lad5_1

--- lab5_1.cpp
+++ lab5_1.cpp
@@ -8,16 +8,32 @@ int main(){
 	
 	string name[N];
 	int age[N];
-	cout << "Name of student = ";
-	cout << "Age of student = ";
+	int i =0;
+	while(i<N) 
+	{cout << "Name of student "<<i+1<<" = ";
+	cin>> name[i];
+	cout << "Age of student "<<i+1<<" = ";
+	cin >>age[i];i++;
+	}
+	
+	
+
 	
 	int key;
 	cout << "--------------------------------------\n";
 	cout << "Enter an age to search: ";
 	cin >> key;
-	cout << "--------------------------------------\n";
 	
-
+	cout << "--------------------------------------\n";
+	int a = 0;
+	while(a<key){
+		if(age[a]==key){
+			cout << name[a] <<"\n";
+		}
+		a++;
+	}
+		
+	
 	
 	cout << "--------------------------------------\n";
 	
